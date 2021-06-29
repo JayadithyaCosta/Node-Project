d6e8e4097f7528e82a9655f3d7837b00f30f30ac
Chathuranga Costa <it17088138@my.sliit.lk>
2021-06-29 23:13:27 +0530

    User cart created and products passing to MongoDB

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoConnect = require('./util/database').mongoConnect;
 
 const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,24 +18,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next)=> {
-    // User.findByPk(1)
-    // .then(user => {
-    //     req.user = user;
-    //     next();
-    // })
-    // .catch(err => console.log(err));
-    next();
-})
-
-// app.use((req, res, next) => {
-//   User.findByPk(1)
-//     .then(user => {
-//       req.user = user;
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('60db07b553e7642d1c770789')
+    .then(user => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -44,4 +35,3 @@ app.use(errorController.get404);
 mongoConnect(() => {
   app.listen(3000);
 });
-
--- models/user.js
+++ models/user.js
@@ -1,19 +1,70 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database')
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define('user', {
+class User {
+  constructor(username, email, cart, id) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id;
+  }
 
-    id : {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
 
-    name: Sequelize.STRING,
-    email: Sequelize.STRING
+  addToCart(product) {
+    
+      let cartProductIndex, updatedCartItems;
+      try {
+        cartProductIndex = this.cart.items.findIndex((cp) => {
+          return cp.productId.toString() === product._id.toString();
+        });
+        updatedCartItems = [...this.cart.items];
+      } catch {
+        cartProductIndex = -1;
+        updatedCartItems = [];
+      } 
+   
+      let newQuantity = 1;
 
-});
+      if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems
+    };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection('users')
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = User;
