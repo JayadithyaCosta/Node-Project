e141705f2085b2be4511ef81820334897aa85d2d
Chathuranga Costa <it17088138@my.sliit.lk>
2021-07-03 13:40:12 +0530

    Mongoose Order POST implemented and works

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Order = require('../models/order');
 
 exports.getProducts = (req, res, next) => {
   Product.find()
@@ -73,7 +74,7 @@ exports.postCart = (req, res, next) => {
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then(result => {
       res.redirect('/cart');
     })
@@ -81,9 +82,24 @@ exports.postCartDeleteProduct = (req, res, next) => {
 };
 
 exports.postOrder = (req, res, next) => {
-  let fetchedCart;
+  
   req.user
-    .addOrder().then(result => {
+    .populate('cart.items.productId')
+    .execPopulate() //populate dont return a promise. execPopulate does
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return {quantity: i.quantity, product: i.productId};
+      });
+      const order = new Order({
+        user:{
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+    });
+    return order.save();
+  })
+    .then(result => {
       res.redirect('/orders');
     })
     .catch(err => console.log(err));
