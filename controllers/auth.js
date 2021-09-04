d91108ed032d961c4cf2564b397da02e1465e187
Chathuranga Costa <ChathurangaC@mbslbank.com>
2021-09-04 18:49:35 +0530

    Section 17 creating reset password form

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const crypto = require("crypto");
+
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const nodemailer = require("nodemailer");
@@ -116,4 +118,77 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy(() => {
     res.redirect("/");
   });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+
+    const token = buffer.toString("hex");
+
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found.");
+          return res.redirect("/reset");
+        }
+
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/");
+        transporter.sendMail({
+          to: req.body.email,
+          from: "shop@node-complete.com",
+          subject: "Password Reset",
+          html: `
+            <p>You requested a password reset!</p>
+            <p>So click this <a href="http://localhost:3000/reset/${token}"/>link</a> to set a new password</p>
+
+          `,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        errorMessage: message,
+        userId: user._id.toString(),
+      });
+    })
+    .catch((err) => console.log(err)); // $gt means greater than
 };
--- models/user.js
+++ models/user.js
@@ -1,84 +1,81 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
 
-    email:{
-        type: String,
-        required: true
-    },
+  password: {
+    type: String,
+    required: true,
+  },
 
-    password: {
-      type: String,
-      required: true
+  resetToken: String,
+  resetTokenExpiration: Date,
 
-    },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
+});
 
-    cart:{
+userSchema.methods.addToCart = function (product) {
+  let cartProductIndex, updatedCartItems;
+  try {
+    cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    updatedCartItems = [...this.cart.items];
+  } catch {
+    cartProductIndex = -1;
+    updatedCartItems = [];
+  }
 
-        items: [
-            {
-                productId: { type: Schema.Types.ObjectId,
-                ref: 'Product', 
-                required: true }, 
-                quantity: { type: Number, required: true 
-            }}
-        ]
-    }
-})
+  let newQuantity = 1;
 
-userSchema.methods.addToCart = function(product){
-    
-    let cartProductIndex, updatedCartItems;
-      try {
-        cartProductIndex = this.cart.items.findIndex((cp) => {
-          return cp.productId.toString() === product._id.toString();
-        });
-        updatedCartItems = [...this.cart.items];
-      } catch {
-        cartProductIndex = -1;
-        updatedCartItems = [];
-      } 
-   
-      let newQuantity = 1;
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
 
-      if (cartProductIndex >= 0) {
-      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
-      updatedCartItems[cartProductIndex].quantity = newQuantity;
-      
-    } else {
-      updatedCartItems.push({
-        productId: product._id,
-        quantity: newQuantity
-      });
-    }
-    const updatedCart = {
-      items: updatedCartItems
-    };
-   
-    this.cart = updatedCart;
-    return this.save();
-
+  this.cart = updatedCart;
+  return this.save();
 };
 
-userSchema.methods.removeFromCart = function(productId){
+userSchema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId.toString() !== productId.toString();
+  });
 
-      const updatedCartItems = this.cart.items.filter(item => {
-        return item.productId.toString() !== productId.toString();
-      });
-      
-      this.cart.items = updatedCartItems;
-      return this.save();
-}
-
-userSchema.methods.clearCart = function(){
-  this.cart = {items: []};
+  this.cart.items = updatedCartItems;
   return this.save();
-}
+};
 
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
+  return this.save();
+};
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
 
 // const mongodb = require('mongodb');
 // const getDb = require('../util/database').getDb;
@@ -98,7 +95,6 @@ module.exports = mongoose.model('User', userSchema);
 //     return db.collection('users').insertOne(this);
 //   }
 
-
 //   addToCart(product) {
 
 //       let cartProductIndex, updatedCartItems;
@@ -110,14 +106,14 @@ module.exports = mongoose.model('User', userSchema);
 //       } catch {
 //         cartProductIndex = -1;
 //         updatedCartItems = [];
-//       } 
-   
+//       }
+
 //       let newQuantity = 1;
 
 //       if (cartProductIndex >= 0) {
 //       newQuantity = this.cart.items[cartProductIndex].quantity + 1;
 //       updatedCartItems[cartProductIndex].quantity = newQuantity;
-      
+
 //     } else {
 //       updatedCartItems.push({
 //         productId: new ObjectId(product._id),
@@ -157,7 +153,6 @@ module.exports = mongoose.model('User', userSchema);
 //       });
 //   }
 
-
 //   deleteItemFromCart(productId){
 //     const db = getDb();
 //     const updatedCartItems = this.cart.items.filter(item => {
@@ -187,7 +182,7 @@ module.exports = mongoose.model('User', userSchema);
 //       }
 
 //       return db.collection('orders').insertOne(order);
-//     }) 
+//     })
 
 //     .then(result => {
 //       this.cart = {items: []};
@@ -202,14 +197,13 @@ module.exports = mongoose.model('User', userSchema);
 
 //   }
 
-
 //   getOrders(){
 
 //     const db = getDb();
 //     return db.collection('orders')
 //     .find({'user._id': new ObjectId(this._id)}).toArray();
 //   }
-  
+
 //   static findById(userId) {
 //     const db = getDb();
 //     return db
